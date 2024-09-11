0b05c4838481b3f4fe6d9ae17a3958b1dad7ae27
Elsera05 <achmadrasyid53@gmail.com>
2024-09-11 16:21:52 +0700

    CICD 2

--- utils/currencyFormat.js
+++
@@ -1,7 +0,0 @@
-const formatCurrency = new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-  });
-  
-  export default formatIDR = (num) => formatCurrency.format(num)
-  
